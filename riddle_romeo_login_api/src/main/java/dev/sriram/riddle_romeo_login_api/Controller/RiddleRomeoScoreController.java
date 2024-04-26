7414c3e8641bbbd58b869fdb6e7904c1f40152c2
Sriram484 <116054870+Sriram484@users.noreply.github.com>
2024-04-26 07:03:35 +0530

    debugged

--- riddle_romeo_login_api/src/main/java/dev/sriram/riddle_romeo_login_api/Controller/RiddleRomeoScoreController.java
+++ riddle_romeo_login_api/src/main/java/dev/sriram/riddle_romeo_login_api/Controller/RiddleRomeoScoreController.java
@@ -16,7 +16,7 @@ import dev.sriram.riddle_romeo_login_api.Service.RiddleRomeoScoreService;
 
 
 @RestController
-@CrossOrigin(origins = "hhttps://papaya-caramel-1b517e.netlify.app/")
+@CrossOrigin(origins = "https://papaya-caramel-1b517e.netlify.app/")
 @RequestMapping("/api/v1/score")
 public class RiddleRomeoScoreController {
     
